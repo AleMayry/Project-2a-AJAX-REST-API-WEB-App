d2ce3524e51f44d35b1fd99d61608a99da8b5037
Aleksi Mäyry <ale.mayry@gmail.com>
2023-04-19 22:05:38 +0300

    3. Commit

--- 2aScript.js
+++ 2aScript.js
@@ -1,13 +1,14 @@
 /**************
  * AJAX call to an API
- */
-let countryList = document.querySelector('.countries');
+ 
+function getCountryInfo(country){
+    let countryList = document.querySelector('.countries');
 
 //1. create an XMLHttpRequest Object 
-let request = new XMLHttpRequest()
+let request = new XMLHttpRequest();
 
 //2. create the request
-request.open('GET','https://restcountries.com/v3.1/name/usa');
+request.open('GET','https://restcountries.com/v3.1/name/'+country);
 
 //3. Send the request
 
@@ -15,9 +16,16 @@ request.send();
 
 request.addEventListener('load', function(){
     //convert JSON string to JavaScript object
-    let [data] = JSON.parse(request.responseText);
+    let[data] = JSON.parse(request.responseText);
     console.log(data);
 
-    const html = 
-    '<article class="country"> <img class="country__img" src="${data.flag}" /> <div class="country__data"> <h3 class="country__name">COUNTRY</h3> <h4 class="country__region">REGION</h4> <p class="country__row"><span></span>POPULATION</p> <p class="country__row"><span></span>LANGUAGE</p> <p class="country__row"><span></span><b>CURRENCY</b></p> </div> </article>';
+    const html = '<article class="country"> <img class="country__img" src="$[data.flag]" > <div class="country__data"> <h3 class="country__name">${data.name}</h3> <h4 class="country__region">${data.region}</h4> <p class="country__row"><span></span>${data.population}</p> <p class="country__row"><span></span>${data.languages[0].name}</p> <p class="country__row"><span></span><b>${data.currencies[0].name}</b></p> </div> </article>';
+
+    countryList.insertAdjacentHTML('beforeend', html)
 })
+}
+getCountryInfo('usa');
+getCountryInfo('germany');
+getCountryInfo('brazil'); */
+
+const countries=document.querySelector(".countries");
