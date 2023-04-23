be8eb39ed81745bdb62fd1fa5c290ba53ec6ca2d
Aleksi Mäyry <ale.mayry@gmail.com>
2023-04-23 21:13:54 +0300

    Functionality Commit

--- 2aScript.js
+++ 2aScript.js
@@ -1,38 +1,10 @@
-/*****
-let searchBtn = document.getElementById("search-btn")
-;
-let countryInp = document.getElementById
-("country-inp");
-searchBtn.addEventListener("click", () => {
-    let countryName = "India";
-    let finalURL = 
-    `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL);
-    fetch(finalURL).then((response) => response.json()).then((data) => {
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies[0]))
-        ;
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(Object.values(data[0].languages).toString().split(",").join(", ")
-        );
-        result.innerHTML = `
-        <img src="${data[0].flags.svg}"
-        class="flag-img">
-        `;
-    });
-}); */
-
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
-  let countryName = countryInp.value;
-  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-  console.log(finalURL);
-  fetch(finalURL)
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+    fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
       //   console.log(data[0]);
